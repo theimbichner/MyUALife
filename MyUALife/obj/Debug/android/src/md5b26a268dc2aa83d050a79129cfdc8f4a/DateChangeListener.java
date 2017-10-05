3eaa597f0e14a12de31b6bdbae5c744937c4a4e2
Taylor Heimbichner <theimbichner@email.arizona.edu>
2017-10-04 19:59:44 -0700

    Initial commit

---
+++ MyUALife/obj/Debug/android/src/md5b26a268dc2aa83d050a79129cfdc8f4a/DateChangeListener.java
@@ -0,0 +1,48 @@
+package md5b26a268dc2aa83d050a79129cfdc8f4a;
+
+
+public class DateChangeListener
+	extends java.lang.Object
+	implements
+		mono.android.IGCUserPeer,
+		android.widget.CalendarView.OnDateChangeListener
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onSelectedDayChange:(Landroid/widget/CalendarView;III)V:GetOnSelectedDayChange_Landroid_widget_CalendarView_IIIHandler:Android.Widget.CalendarView/IOnDateChangeListenerInvoker, Mono.Android, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null\n" +
+			"";
+		mono.android.Runtime.register ("MyUALife.DateChangeListener, MyUALife, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", DateChangeListener.class, __md_methods);
+	}
+
+
+	public DateChangeListener () throws java.lang.Throwable
+	{
+		super ();
+		if (getClass () == DateChangeListener.class)
+			mono.android.TypeManager.Activate ("MyUALife.DateChangeListener, MyUALife, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onSelectedDayChange (android.widget.CalendarView p0, int p1, int p2, int p3)
+	{
+		n_onSelectedDayChange (p0, p1, p2, p3);
+	}
+
+	private native void n_onSelectedDayChange (android.widget.CalendarView p0, int p1, int p2, int p3);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
