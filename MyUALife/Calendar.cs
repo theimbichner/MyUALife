cc6e324cdfe9fc22bf7f64fa69ae565047e08b84
Daniel Alexander <daniel640@email.arizona.edu>
2017-10-09 00:33:54 -0700

    Added Basic Calendar, Event Classes

---
+++ MyUALife/Calendar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Calendar
+{
+    List<Event> events = new List<Event>();
+
+    public Calendar()
+    {
+
+    }
+
+    public Calendar(List<Event> events)
+    {
+        this.events = events;
+    }
+
+    public void AddEvent(Event e)
+    {
+        events.Add(e);
+    }
+
+    public bool RemoveEvent(Event e)
+    {
+        return events.Remove(e);
+    }
+
+    public List<Event> GetEventsInRange(DateTime start, DateTime end)
+    {
+        List<Event> output = new List<Event>();
+        foreach(Event e in events)
+        {
+            if (e.StartTime <= end && e.EndTime >= start)
+            {
+                output.Add(e);
+            }
+        }
+        return output;
+    }
+
+
+
+
+}
---
+++ MyUALife/Event.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Event
+{
+    public DateTime StartTime
+    {
+        get;
+        set;
+    }
+
+    public DateTime EndTime
+    {
+        get;
+        set;
+    }
+
+    public String Name
+    {
+        get;
+        set;
+    }
+
+    public Event(String name, DateTime start, DateTime end)
+    {
+        Name = name;
+        StartTime = start;
+        EndTime = end;
+    }
+}
