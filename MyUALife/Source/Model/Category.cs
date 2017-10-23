a3545f30ae0213d4cf3d4e6a823543e6968914e0
CogentInvalid <pchipman@email.arizona.edu>
2017-10-23 15:09:20 -0700

    Added EventType priority field

--- MyUALife/Source/Model/Category.cs
+++ MyUALife/Source/Model/Category.cs
@@ -1,16 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 public class Category
 {
-    public static EventType classTime = new EventType("ClassTime", false);
-    public static EventType studyTime = new EventType("StudyTime", true);
-    public static EventType homework = new EventType("Homework", true);
-    public static EventType appointment = new EventType("Appointment", false);
-    public static EventType recreation = new EventType("Recreation", true);
+    public static EventType classTime   = new EventType("ClassTime", 0, false);
+    public static EventType appointment = new EventType("Appointment", 1, false);
+    public static EventType homework    = new EventType("Homework", 2, true);
+    public static EventType studyTime   = new EventType("StudyTime", 3, true);
+    public static EventType recreation  = new EventType("Recreation", 4, true);
+    public static EventType freeTime    = new EventType("Free Time", 5, true);
 
-    public static EventType freeTime = new EventType("Free Time", true);
-
-    List<EventType> theTypes = new List<EventType>(new EventType[] { classTime,
+    public List<EventType> creatableTypes = new List<EventType>(new EventType[] { classTime,
         studyTime, homework, appointment, recreation });
 }
--- MyUALife/Source/Model/EventType.cs
+++ MyUALife/Source/Model/EventType.cs
@@ -4,10 +4,12 @@ public struct EventType
 {
     String name;
     bool editable;
+    int priority;
 
-    public EventType(String str, bool ed)
+    public EventType(String str, int priority, bool ed)
     {
         name = str;
+        this.priority = priority;
         editable = ed;
     }
 }
